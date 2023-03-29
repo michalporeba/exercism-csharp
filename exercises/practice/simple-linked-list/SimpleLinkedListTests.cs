2974e5671ee0f71167d9de46b88e8f830dd31f31
Michał Poręba <michalporeba@gmail.com>
2023-03-29 22:19:22 +0100

    udoing the test/implementation mixup

--- exercises/practice/simple-linked-list/SimpleLinkedListTests.cs
+++ exercises/practice/simple-linked-list/SimpleLinkedListTests.cs
@@ -1,6 +1,6 @@
 using System.Linq;
-using System.Collections.Generic;
 using Xunit;
+using System.Collections.Generic;
 
 public class SimpleLinkedListTests
 {
@@ -8,79 +8,58 @@ public class SimpleLinkedListTests
     public void Single_item_list_value()
     {
         var list = new SimpleLinkedList<int>(1);
-        Assert.Equal(1, list.Value);
+        Assert.Equal(1, list.Remove());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Single_item_list_has_no_next_item()
-    {
-        var list = new SimpleLinkedList<int>(1);
-        Assert.Null(list.Next);
-    }
-
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Two_item_list_first_value()
     {
-        var list = new SimpleLinkedList<int>(2).Add(1);
-        Assert.Equal(2, list.Value);
+        var list = new SimpleLinkedList<int>(1, 2);
+        Assert.Equal(2, list.Remove());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Two_item_list_second_value()
     {
-        var list = new SimpleLinkedList<int>(2).Add(1);
-        Assert.Equal(1, list.Next.Value);
+        var list = new SimpleLinkedList<int>(1, 2);
+        list.Remove();
+        Assert.Equal(1, list.Remove());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Two_item_list_second_item_has_no_next()
+    [Fact]
+    public void Multivalue_initialisation()
     {
-        var list = new SimpleLinkedList<int>(2).Add(1);
-        Assert.Null(list.Next.Next);
+        var values = new SimpleLinkedList<int>(2, 1, 3);
+        Assert.Equal(new[] { 3, 1, 2 }, values);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Implements_enumerable()
-    {
-        var list = new SimpleLinkedList<int>(2).Add(1).Add(3);
-        var enumerable = Assert.IsAssignableFrom<IEnumerable<int>>(list);
-        var enumerator = enumerable.GetEnumerator();
-        Assert.True(enumerator.MoveNext());
-        Assert.Equal(2, enumerator.Current);
-        Assert.True(enumerator.MoveNext());
-        Assert.Equal(1, enumerator.Current);
-        Assert.True(enumerator.MoveNext());
-        Assert.Equal(3, enumerator.Current);
-        Assert.False(enumerator.MoveNext());
-    }
-
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void From_enumerable()
     {
         var list = new SimpleLinkedList<int>(new[] { 11, 7, 5, 3, 2 });
-        Assert.Equal(11, list.Value);
-        Assert.Equal(7, list.Next.Value);
-        Assert.Equal(5, list.Next.Next.Value);
-        Assert.Equal(3, list.Next.Next.Next.Value);
-        Assert.Equal(2, list.Next.Next.Next.Next.Value);
+        Assert.Equal(2, list.Remove());
+        Assert.Equal(3, list.Remove());
+        Assert.Equal(5, list.Remove());
+        Assert.Equal(7, list.Remove());
+        Assert.Equal(11, list.Remove());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Reverse()
+    [Fact]
+    public void Reverse_enumerable()
     {
         var values = Enumerable.Range(1, 5).ToArray();
         var list = new SimpleLinkedList<int>(values);
         var enumerable = Assert.IsAssignableFrom<IEnumerable<int>>(list);    
         var reversed = enumerable.Reverse();
-        Assert.Equal(values.Reverse(), reversed);
+        Assert.Equal(values, reversed);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Roundtrip()
     {
         var values = Enumerable.Range(1, 7);
-        var list = new SimpleLinkedList<int>(values);
+        var list = new SimpleLinkedList<int>(values.ToArray());
         var enumerable = Assert.IsAssignableFrom<IEnumerable<int>>(list);    
-        Assert.Equal(values, enumerable);
+        Assert.Equal(values.Reverse(), enumerable);
     }
 }
